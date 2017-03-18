8f3a0bd4863ee92246bb1879854b2e558ca26657
Atsushi Kimura <sudachi808@gmail.com>
2017-03-19 00:32:59 +0900

    シウマイ貼り付け処理を（仮）実装

--- app/js/controllers/CalcBoxController.js
+++ app/js/controllers/CalcBoxController.js
@@ -3,8 +3,25 @@
  */
 angular.module('app', [])
 .controller('CalcBoxController', function($element) {
+
     var self = this;
+
+    self.items = [];
+
     self.toggleOpen = function() {
-        console.log('toggleOpen', $element.toggleClass('calc-box-open'));
+        $element.toggleClass('calc-box-open');
     };
+
+    self.clickBtn = function(event, index) {
+        self.items[index].sticked = true;
+    };
+
+    /**
+     * コンストラクタ
+     */
+    (function(){
+        for (var i = 0 ; i < 4 * 5 ; i++) {
+            self.items.push({ sticked : false });
+        }
+    })();
 });
